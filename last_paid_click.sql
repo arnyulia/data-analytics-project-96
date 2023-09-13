17bc9ef899e72b376e516e3904c0cf1b2177e9ce
arnyulia <134854672+arnyulia@users.noreply.github.com>
2023-09-13 13:59:42 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,12 +1,23 @@
 "last_paid_click"
 
-with tab as(
-select distinct on (s.visitor_id) s.visitor_id, visit_date, source as utm_source, medium as utm_medium, campaign as utm_campaign, 
-lead_id, created_at, amount, closing_reason, status_id
-from sessions s
-left join leads l
-on s.visitor_id=l.visitor_id 
-where medium <> 'organic'
-order by s.visitor_id, visit_date desc) 
-select * from tab 
+with tab as (
+    select distinct on (s.visitor_id)
+        s.visitor_id,
+        visit_date,
+        source as utm_source,
+        medium as utm_medium,
+        campaign as utm_campaign,
+        lead_id,
+        created_at,
+        amount,
+        closing_reason,
+        status_id
+    from sessions as s
+    left join leads as l
+        on s.visitor_id = l.visitor_id
+    where medium != 'organic'
+    order by s.visitor_id asc, visit_date desc
+)
+
+select * from tab
 order by visit_date, utm_source, utm_medium, utm_campaign;
