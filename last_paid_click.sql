1e896f5e00c2c25ef773ec45b79a5e01aa6508e9
arnyulia <134854672+arnyulia@users.noreply.github.com>
2023-09-25 10:22:13 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,23 +1,43 @@
 "last_paid_click"
 
-with tab as (
-    select distinct on (s.visitor_id)
+WITH tab AS (
+    SELECT
         s.visitor_id,
-        visit_date,
-        source as utm_source,
-        medium as utm_medium,
-        campaign as utm_campaign,
-        lead_id,
-        created_at,
-        amount,
-        closing_reason,
-        status_id
-    from sessions as s
-    left join leads as l
-        on s.visitor_id = l.visitor_id
-    where medium != 'organic'
-    order by s.visitor_id asc, visit_date desc
-)
-
-select * from tab
-order by visit_date, utm_source, utm_medium, utm_campaign;
+        s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        s.content AS utm_content,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id
+    FROM sessions s
+    LEFT JOIN leads AS l
+        ON s.visitor_id = l.visitor_id AND s.visit_date <= l.created_at),
+tab2 AS (
+    SELECT DISTINCT ON (a.visitor_id)
+        a.visitor_id,
+        b.utm_campaign,
+        b.utm_content,
+        b.lead_id,
+        b.created_at,
+        b.amount,
+        b.closing_reason,
+        b.status_id,
+        coalesce(b.visit_date, a.visit_date) AS visit_date,
+        coalesce(b.utm_source, a.utm_source) AS utm_source,
+        coalesce(b.utm_medium, a.utm_medium) AS utm_medium
+    FROM tab AS a
+    LEFT JOIN tab AS b
+        ON a.visitor_id = b.visitor_id AND b.utm_medium != 'organic'
+    ORDER BY a.visitor_id, b.visit_date DESC)
+    SELECT *
+FROM tab2
+ORDER BY
+    amount DESC NULLS LAST,
+    date_trunc('day', visit_date),
+    utm_source,
+    utm_medium,
+    utm_campaign;
