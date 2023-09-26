c92e6c233fd16d79cc0eb99bcc03de27bf43a472
arnyulia <134854672+arnyulia@users.noreply.github.com>
2023-09-26 12:04:01 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,34 +1,39 @@
 "last_paid_click"
 
-with tab as (select
-    s.visitor_id,
-    visit_date,
-    source,
-    medium,
-    campaign,
-    created_at,
-    amount,
-    closing_reason,
-    status_id,
-    lead_id,
-    ROW_NUMBER()
-        over (partition by s.visitor_id order by visit_date desc) as rn
-from sessions s
-left join leads l
-    on s.visitor_id = l.visitor_id
-    and visit_date <= created_at
-WHERE medium <> 'organic')
-select
-        visitor_id,
+with tab as (
+    select
+        s.visitor_id,
         visit_date,
-        source as utm_source,
-        medium as utm_medium,
-        campaign as utm_campaign,
-        lead_id,
+        source,
+        medium,
+        campaign,
         created_at,
         amount,
         closing_reason,
-        status_id
-    from tab
-    where tab.rn = 1
-   order by amount desc NULLS LAST, visit_date, 4,5,6;
+        status_id,
+        lead_id,
+        ROW_NUMBER()
+            over (partition by s.visitor_id order by visit_date desc)
+        as rn
+    from sessions as s
+    left join leads as l
+        on
+            s.visitor_id = l.visitor_id
+            and visit_date <= created_at
+    where medium != 'organic'
+)
+
+select
+    tab.visitor_id,
+    tab.visit_date,
+    tab.source as utm_source,
+    tab.medium as utm_medium,
+    tab.campaign as utm_campaign,
+    tab.lead_id,
+    tab.created_at,
+    tab.amount,
+    tab.closing_reason,
+    tab.status_id
+from tab
+where tab.rn = 1
+order by tab.amount desc nulls last, tab.visit_date asc, 4, 5, 6;
