659e6adebde10b05cb3677cf010b4b433d31e5c0
arnyulia <134854672+arnyulia@users.noreply.github.com>
2023-09-26 11:53:01 +0300

    Update aggregate_last_paid_click.sql

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,4 +1,4 @@
-with tab as (select
+ with tab as (select
     s.visitor_id,
     visit_date,
     source,
@@ -17,87 +17,47 @@ left join leads l
     and visit_date <= created_at
 WHERE medium <> 'organic'),
 tab2 as (select
-        tab.visitor_id,
         tab.source as utm_source,
         tab.medium as utm_medium,
         tab.campaign as utm_campaign,
-        tab.created_at,
-        tab.amount,
-        tab.closing_reason,
-        tab.status_id,
-        date_trunc('day', tab.visit_date) as visit_date,
-        lead_id
-    from tab
-    where tab.rn = 1),
-tab3 as (select
-        visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
         count(visitor_id) as visitors_count,
         sum(case when lead_id is not null then 1 else 0 end) as leads_count,
         sum(case when status_id = 142 then 1 else 0 end) as purchases_count,
-        sum(amount) as revenue
-    from tab2
-    group by 1, 2, 3, 4),
+        sum(amount) as revenue,
+        to_char(date_trunc('day', tab.visit_date), 'YY-MM-DD') as visit_date
+    from tab
+    where tab.rn = 1
+    group by 1,2,3,8),
 tab4 as (select
-        campaign_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        daily_spent
-    from vk_ads union all
-    select
-        campaign_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        daily_spent
-    from ya_ads),
-tab5 as (select
-        campaign_date as visit_date,
+        to_char(campaign_date, 'YY-MM-DD') as visit_date,
         utm_source,
         utm_medium,
         utm_campaign,
         sum(daily_spent) as total_cost
-    from tab4
-    group by
-        visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign),
-tab6 as (select
-        visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        null as revenue,
-        null as visitors_count,
-        null as leads_count,
-        null as purchases_count,
-        total_cost
-    from tab5
+    from vk_ads 
+    group by 1,2,3,4
     union all
     select
-        visit_date,
+        to_char(campaign_date, 'YY-MM-DD') as visit_date,
         utm_source,
         utm_medium,
         utm_campaign,
-        revenue,
+        sum(daily_spent) as total_cost
+    from ya_ads
+    group by 1,2,3,4)    
+select
+        tab2.visit_date,
+        tab2.utm_source,
+        tab2.utm_medium,
+        tab2.utm_campaign,
         visitors_count,
+        total_cost,
         leads_count,
         purchases_count,
-        null as total_cost
-    from tab3)
-select
-    visit_date,
-    utm_source,
-    utm_medium,
-    utm_campaign,
-    sum(coalesce(visitors_count, 0)) as visitors_count,
-    sum(coalesce(total_cost, 0)) as total_cost,
-    sum(coalesce(leads_count, 0)) as leads_count,
-    sum(coalesce(purchases_count, 0)) as purchases_count,
-    sum(coalesce(revenue, 0)) as revenue from tab6
-group by 1, 2, 3, 4
-order by 9 desc, 1, 5 desc,2, 3, 4 limit 15;
+        revenue
+    from tab2 left join tab4 on tab2.visit_date=tab4.visit_date 
+        AND tab2.utm_source = tab4.utm_source
+        AND tab2.utm_medium = tab4.utm_medium
+        AND tab2.utm_campaign = tab4.utm_campaign
+     group by 1,2,3,4,5,6,7,8,9 
+     order by revenue desc nulls last, visit_date, visitors_count desc, 2,3,4;
